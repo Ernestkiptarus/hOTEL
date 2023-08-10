df849774349cdce398c443202d29e4c4f6548118
Ernestkiptarus <kiptarusernest259@gmail.comls>
2023-08-10 17:00:40 +0300

    refator

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Fetch data from the API and populate the room list
+// Function to fetch room data from JSON file
 function fetchRooms() {
-  fetch('Json-server --watch db.json')
+  fetch('rooms.json') // Assuming your JSON file is named "rooms.json"
     .then((response) => response.json())
     .then((data) => {
       const roomListSection = document.getElementById('room-list');
@@ -22,11 +22,44 @@ function fetchRooms() {
     .catch((error) => console.error('Error fetching rooms:', error));
 }
 
-// Other functions and event listeners...
+// Function to handle reservation form submission
+function handleReservationFormSubmit(event) {
+  event.preventDefault();
 
-// Attach event listener to the reservation form submit button
-const reservationForm = document.getElementById('reservation-form');
-reservationForm.addEventListener('submit', handleReservationFormSubmit);
+  const reservationForm = event.target;
+  const guestName = reservationForm.elements['guest-name'].value;
+  const guestEmail = reservationForm.elements['guest-email'].value;
+  const selectedRoomId = reservationForm.elements['room'].value;
+  const checkInDate = reservationForm.elements['check-in-date'].value;
+  const checkOutDate = reservationForm.elements['check-out-date'].value;
+
+  // Here, you can send the reservation data to the API and handle the response accordingly
+  // For simplicity, we'll just log the reservation data for now
+  const reservationData = {
+    guestName,
+    guestEmail,
+    roomId: selectedRoomId,
+    checkInDate,
+    checkOutDate,
+  };
+
+  console.log('Reservation data:', reservationData);
+
+  // Clear the form fields after submission
+  reservationForm.reset();
+}
+
+// Function to show/hide sections based on user interactions (client-side routing)
+function showSection(sectionId) {
+  const sections = document.querySelectorAll('main section');
+  sections.forEach((section) => {
+    if (section.id === sectionId) {
+      section.style.display = 'block';
+    } else {
+      section.style.display = 'none';
+    }
+  });
+}
 
 // Add event listeners for navigation links
 document.getElementById('nav-room-list').addEventListener('click', () => {
@@ -41,5 +74,9 @@ document.getElementById('nav-booking-history').addEventListener('click', () => {
   showSection('booking-history');
 });
 
-// On page load, fetch the rooms data and populate the room list
+// Attach event listener to the reservation form submit button
+const reservationForm = document.getElementById('reservation-form');
+reservationForm.addEventListener('submit', handleReservationFormSubmit);
+
+// Run fetchRooms function on page load
 document.addEventListener('DOMContentLoaded', fetchRooms);
